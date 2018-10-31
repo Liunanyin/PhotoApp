95816b42c9c3358d2dbb7e663290eca0c23c14be
LIUNAN YIN <liunanyin@LIUNANs-MacBook-Pro.local>
2018-10-31 15:05:49 -0700

    created a new project

---
+++ PhotoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoApp
+//
+//  Created by LIUNAN YIN on 2018-10-31.
+//  Copyright © 2018 LIUNAN YIN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
