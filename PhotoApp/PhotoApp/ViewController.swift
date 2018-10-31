0f2e6ce81616e3b96f59f435e098259f4bde97f4
LIUNAN YIN <liunanyin@LIUNANs-MacBook-Pro.local>
2018-10-31 16:24:41 -0700

    finished append image array

--- PhotoApp/PhotoApp/ViewController.swift
+++ PhotoApp/PhotoApp/ViewController.swift
@@ -11,17 +11,50 @@ import UIKit
 class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate{
     @IBOutlet weak var collectionView: UICollectionView!
     
+    let reuseIdentifier = "cell"
+    var array = [Photo]()
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        <#code#>
+        return self.array.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        <#code#>
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath as IndexPath) as! CollectionViewCell
+        
+        cell.myImage.image = array[indexPath.item].image
+       
+    
+        cell.layer.borderColor = UIColor.white.cgColor
+        cell.layer.cornerRadius = 8
+        cell.layer.borderWidth = 0
+        cell.layer.backgroundColor = UIColor.white.cgColor
+        cell.myImage.layer.borderColor = UIColor.white.cgColor
+        cell.myImage.layer.borderWidth = 1
+        cell.myImage.layer.cornerRadius = 8
+        return cell
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
+        array.append(Photo(name: "sunrise", date: 20161007, image: #imageLiteral(resourceName: "sunrise")))
+        array.append(Photo(name: "sunset", date: 20160819, image: #imageLiteral(resourceName: "sunset")))
+        array.append(Photo(name: "sea", date: 20151127, image: #imageLiteral(resourceName: "sea")))
+        array.append(Photo(name: "mountain", date: 20161007, image: #imageLiteral(resourceName: "mountain")))
+        array.append(Photo(name: "lake", date: 20161007, image: #imageLiteral(resourceName: "lake")))
+        array.append(Photo(name: "island", date: 20161007, image: #imageLiteral(resourceName: "island")))
+        array.append(Photo(name: "flower", date: 20161007, image: #imageLiteral(resourceName: "flower")))
+        array.append(Photo(name: "blossom", date: 20161007, image: #imageLiteral(resourceName: "blossom")))
+        array.append(Photo(name: "beach", date: 20161007, image: #imageLiteral(resourceName: "beach")))
+        array.append(Photo(name: "tree", date: 20161007, image: #imageLiteral(resourceName: "tree")))
     }
 
 
